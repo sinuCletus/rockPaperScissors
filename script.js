c3682b5e9e26147e61ed2b595d66c6fd6eaa8c4b
sinuCletus <84879210+sinuCletus@users.noreply.github.com>
2022-07-22 14:22:56 -0700

    Modify js script to add 2 functions
    A function to take user inputs for the rock paper scissors game and a
    function to play 5 rounds were created.
    
    The getPlayerSelection() prompts a user to type rock, paper, or scissors
    and returns that in the variable 'input'. input is then modified so that
    the prompt is always returned as lowercase letters. This will ensure
    that the user input is case insensitive and matches with one of three
    choices in the choices array. I used a while loop to validate the user
    input to ensure that it is spelled correctly and also not an empty
     string.
    
    The playRound() still has work to be done on it. It takes two parameters
    of playerSelection and computerSelection. These two parameters as of now
    appear as variables  within the function that are both passed the
    functions that take both the user's input and the computer's random
    selected choice. It prints to the console over five iterations using
    a for loop. I hope to use this to create five rounds and pass the
    parameters/functions each round and then add code to compare and declare
    a winner for each round.

--- script.js
+++ script.js
@@ -2,29 +2,46 @@
  ------------------------------------------------- 
 Problem: Create Rock-Paper-Scissors Game
 
+                        Plan
 Crucial functions:
 
 create a function to get the computer to play
+create function to capture player selection
 create function to play a single round
 create function to play a game of 5 rounds
+create a function to reset after either party reaches 5
 
+Method:
 
  ------------------------------------------------- 
 ------------------------------------------------- */
 
 //VARIABLES
-let choice = ['rock', 'paper', 'scissors'];
-let computerSelection = getComputerChoice();
-//let playerSelection = something the player chooses;
-let winner = '';
+let choices = ['rock', 'paper', 'scissors'];
 
+//FUNCTIONS
 /*
 name: getComputerChoice
-param: rock, paper, scissors
+purpose: return a random selection from the choice array to use as the computer's rock-paper-scissors selection
+param: N/A
 return: choice
 */
 function getComputerChoice() {
-    return choice[Math.floor(Math.random()*choice.length)];
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+/*
+name: getPlayerChoice
+param: N/A
+return: input
+*/
+function getPlayerSelection() {
+    let input = prompt('Choose rock, paper, or scissors?');
+    input = input.toLowerCase();
+    while (input != 'rock' & input != 'paper' && input != 'scissors') {
+        input = prompt('Please defend yourself with rock, paper, or scissors. Hint: Check spelling.');
+    }
+    return input;
 }
 
 /*
@@ -33,5 +50,12 @@ param: playerSelection, computerSelection
 return: winner
 */
 function playRound(playerSelection, computerSelection) {
-    
+    for (i=1; i<=5; i++) {
+        console.log("Round " + i);
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerSelection();
+        console.log("Computer Chooses: " + computerSelection);
+        console.log("You selected: " + playerSelection);
+    }
 }
+playRound();
